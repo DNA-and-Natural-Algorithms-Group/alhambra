eb640f505f90648add2c5df4285e89a0b7ef7b08
Constantine Evans <const@costinet.org>
2021-08-05 20:53:26 +0100

    move to setuptools_scm

--- src/alhambra/__init__.py
+++ src/alhambra/__init__.py
@@ -1,4 +1,3 @@
-__version__ = "2.0.0a4"
 __all__ = ["TileSet", "Tile", "TileList", "Glue", "GlueList"]
 
 from . import classes
@@ -6,3 +5,13 @@ from . import classes
 from .tilesets import TileSet
 from .tiles import Tile, TileList
 from .glues import Glue, GlueList
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "qslib"
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
