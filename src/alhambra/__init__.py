1cdc96c8da8ee8caf1914e16be99c8384469bcae
Constantine Evans <const@costinet.org>
2021-08-06 00:47:35 +0100

    version write_to

--- src/alhambra/__init__.py
+++ src/alhambra/__init__.py
@@ -1,17 +1,9 @@
 __all__ = ["TileSet", "Tile", "TileList", "Glue", "GlueList"]
-
+# Hi
 from . import classes
 
 from .tilesets import TileSet
 from .tiles import Tile, TileList
 from .glues import Glue, GlueList
-from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = "alhambra"
-    __version__ = version(dist_name)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+from ._version import version as __version__  # type: ignore
