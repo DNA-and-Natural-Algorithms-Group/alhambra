b46b17af2e7604dc5f75528e8f0b78b054beec63
Constantine Evans <const@costinet.org>
2021-08-05 21:31:57 +0100

    actually give alhambra version...

--- src/alhambra/__init__.py
+++ src/alhambra/__init__.py
@@ -9,7 +9,7 @@ from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "qslib"
+    dist_name = "alhambra"
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
