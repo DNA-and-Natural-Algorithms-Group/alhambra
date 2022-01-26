57f462fc8fa84909968615e6616845ba6f5de66a
Constantine Evans <const@costi.eu>
2022-01-26 03:00:21 +0000

    mix improvements

--- tests/test_mixes.py
+++ tests/test_mixes.py
@@ -2,9 +2,22 @@ import re
 import numpy as np
 import pytest
 
-from alhambra.mixes import Component, FixedConcentration, FixedVolume, Mix, MultiFixedVolume, Strand, WellPos, load_reference, nM, uM, ureg
+from alhambra.mixes import (
+    Component,
+    FixedConcentration,
+    FixedVolume,
+    Mix,
+    MultiFixedVolume,
+    Strand,
+    WellPos,
+    load_reference,
+    nM,
+    uM,
+    ureg,
+)
 import itertools
 import pandas as pd
+
 
 def test_wellpos_movement():
     "Ensure WellPos movements are correct, and fail when appropriate."
@@ -13,7 +26,9 @@ def test_wellpos_movement():
     assert WellPos("A12").next_byrow() == WellPos("B1")
     assert WellPos("A12").next_bycol() == WellPos("B12")
 
-    with pytest.raises(ValueError, match=r"Row I \(9\) out of bounds for plate size 96"):
+    with pytest.raises(
+        ValueError, match=r"Row I \(9\) out of bounds for plate size 96"
+    ):
         WellPos("H12").next_byrow()
 
     with pytest.raises(ValueError, match="Column 13 out of bounds for plate size 96"):
@@ -21,9 +36,9 @@ def test_wellpos_movement():
 
     assert WellPos("A12", platesize=384).next_byrow() == "A13"
 
-    assert WellPos("H14", platesize = 384).next_bycol() == "I14"
+    assert WellPos("H14", platesize=384).next_bycol() == "I14"
 
-    assert WellPos("A12", platesize = 384) == WellPos("A12", platesize = 96)
+    assert WellPos("A12", platesize=384) == WellPos("A12", platesize=96)
 
     assert WellPos("D6") == WellPos(4, 6)
 
@@ -31,14 +46,14 @@ def test_wellpos_movement():
 
     assert str(WellPos("D6")) == "D6"
 
-
-    assert repr(WellPos("D8")) == "WellPos(\"D8\")"
+    assert repr(WellPos("D8")) == 'WellPos("D8")'
 
     assert WellPos("C8").key_byrow() == (3, 8)
 
     assert WellPos("C8").key_bycol() == (8, 3)
 
     assert WellPos("D8") == WellPos(WellPos("D8"))
+
 
 def test_invalid_wellrefs():
     with pytest.raises(ValueError):
@@ -67,10 +82,10 @@ def test_all_wellref_96():
     for x, y in itertools.pairwise(allbyrow96):
         assert WellPos(x).next_byrow() == y
 
-
-    allbyrow96 = [f"{r}{c}"  for c in range(1, 13) for r in "ABCDEFGH"]
+    allbyrow96 = [f"{r}{c}" for c in range(1, 13) for r in "ABCDEFGH"]
     for x, y in itertools.pairwise(allbyrow96):
         assert WellPos(x).next_bycol() == y
+
 
 def test_component():
 
@@ -78,23 +93,27 @@ def test_component():
 
     assert Component("test3") == Component("test3")
 
-    assert Component("A", 1*uM) == Component("A", 1000*nM)
+    assert Component("A", 1 * uM) == Component("A", 1000 * nM)
 
-    assert Component("A", 1*uM) != Component("A", 1002*nM)
+    assert Component("A", 1 * uM) != Component("A", 1002 * nM)
 
     assert Component("A") != Strand("A")
 
     assert Component("A") != 5
 
+
 def test_component_allcomps():
-    ac = Component("A", 1*uM).all_components()
+    ac = Component("A", 1 * uM).all_components()
 
     assert len(ac) == 1
-    assert ac.loc["A", "component"] == Component("A", 1*uM)
+    assert ac.loc["A", "component"] == Component("A", 1 * uM)
     assert np.allclose(ac.loc["A", "concentration_nM"], 1000.0)
+
+
 @pytest.fixture
 def reference():
     return load_reference("tests/test_reference.csv")
+
 
 def test_component_with_reference(reference: pd.DataFrame):
     c = Component("comp1")
@@ -105,6 +124,7 @@ def test_component_with_reference(reference: pd.DataFrame):
 
     with pytest.raises(ValueError):
         Component("comp1", ureg.Quantity(150.0, "nM")).with_reference(reference)
+
 
 def test_strand_with_reference(reference: pd.DataFrame):
     c = Strand("strand1")
@@ -120,6 +140,7 @@ def test_strand_with_reference(reference: pd.DataFrame):
     with pytest.raises(ValueError):
         Strand("strand1", sequence="AGCTG").with_reference(reference)
 
+
 def test_a_mix(reference: pd.DataFrame):
     c1 = Component("comp1")
     s1 = Strand("strand1")
@@ -127,10 +148,14 @@ def test_a_mix(reference: pd.DataFrame):
     s3 = Strand("strand3", ureg("1000 nM"), "GGTG")
 
     m = Mix(
-    [MultiFixedVolume([s1, s2, s3], ureg("10 uL"), compact_display=True),
-     FixedConcentration(c1, "100 nM"),
-     FixedVolume(s3, ureg("10 uL"))
-    ], "test", ureg("50 uL"), fixed_concentration='strand3'
+        [
+            MultiFixedVolume([s1, s2, s3], ureg("10 uL"), compact_display=True),
+            FixedConcentration(c1, "100 nM"),
+            FixedVolume(s3, ureg("10 uL")),
+        ],
+        "test",
+        ureg("50 uL"),
+        fixed_concentration="strand3",
     ).with_reference(reference)
 
     assert m.buffer_volume == ureg("5 uL")
@@ -138,7 +163,10 @@ def test_a_mix(reference: pd.DataFrame):
 
     mdt = m._repr_markdown_().splitlines()
 
-    assert re.match(r"Table: Mix: test, Conc: 400.00 nM, Total Vol: 50.00", mdt[0]) is not None
+    assert (
+        re.match(r"Table: Mix: test, Conc: 400.00 nM, Total Vol: 50.00", mdt[0])
+        is not None
+    )
 
     # FIXME: more tests of output
 
