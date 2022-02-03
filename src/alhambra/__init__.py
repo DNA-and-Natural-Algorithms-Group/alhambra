6bf928195dff6d028078ade3236634409c93fff3
Constantine Evans <const@costi.eu>
2022-02-03 15:10:55 +0000

    dev version if not installed

--- src/alhambra/__init__.py
+++ src/alhambra/__init__.py
@@ -1,7 +1,11 @@
 __all__ = ["TileSet", "Tile", "TileList", "Glue", "GlueList"]
 # Hi
 from . import classes
-from ._version import version as __version__  # type: ignore
+
+try:
+    from ._version import version as __version__  # type: ignore
+except:
+    __version__ = "dev"
 from .glues import Glue, GlueList
 from .tiles import Tile, TileList
 from .tilesets import TileSet
